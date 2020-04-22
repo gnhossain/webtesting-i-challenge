62db1cf4b7fa55634a1a2b192a597f2f5f41039e
gul hossain <spicymangojungle@gmail.com>
2020-04-22 15:58:15 -0700

    enhancer update

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,6 +1,13 @@
-const enhancer = require('./enhancer.js');
+
+const { succeed, fail, repair, get }= require('./enhancer.js');
 // test away!
 
 it ('should run the test', function(){
     expect(true).toBe(true)
 })
+
+// describe(".succeed()", function() {
+//     it("should succeed", function(){
+//         expect(succeed({name:"Item 1", enhancement:19, durability:100 }).toBe({name:"Item 1", enhancement:20, durability:100}))
+//     })
+// })
